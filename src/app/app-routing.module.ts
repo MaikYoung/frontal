64850761153f7506c6755899e17be62833546806
Mike Young <maikelym1990@gmail.com>
2021-05-16 14:01:55 +0200

    add navigation and component between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { AcademyComponent } from './pages/academy/academy.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { BiographyComponent } from './pages/biography/biography.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
+import { ServicioComponent } from './pages/servicio/servicio.component';
 
 const routes: Routes = [
-  {path: '', component: MainComponent}
+  {
+    path: '', 
+    component: MainComponent
+  },
+  {
+    path: 'academy',
+    component: AcademyComponent
+  },
+  {
+    path: 'contact',
+    component: ContactoComponent
+  },
+  {
+    path: 'biography',
+    component: BiographyComponent
+  },
+  {
+    path: 'courses',
+    component: CursosComponent
+  },
+  {
+    path: 'services',
+    component: ServicioComponent
+  }
 ];
 
 @NgModule({
