32ca4200d7bf702ff547ad8b56a0eef1ce54a96c
Mike Young <maikelym1990@gmail.com>
2021-05-18 14:13:04 +0200

    finish structure on bio page

--- src/app/pages/biography/biography.component.ts
+++ src/app/pages/biography/biography.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./biography.component.scss']
 })
 export class BiographyComponent implements OnInit {
+  show:string = '' 
 
   constructor() { }
 
   ngOnInit() {
+    this.show = 'bio'
+    console.log("entrando", this.show);
+    
+  }
+
+  public setShowValue(val:string){
+    this.show = val
+    console.log(this.show);
+    
   }
 
 }
