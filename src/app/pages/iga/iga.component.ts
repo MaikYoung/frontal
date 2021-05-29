b13a59201b134b370c802520b73f923016c77d33
Mike Young <maikelym1990@gmail.com>
2021-05-29 13:55:00 +0200

    added iga page

---
+++ src/app/pages/iga/iga.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iga',
+  templateUrl: './iga.component.html',
+  styleUrls: ['./iga.component.scss']
+})
+export class IgaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
