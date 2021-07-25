1e1e309ec15c1f04c753f997d1255aac9e77e624
Mike Young <maikelym1990@gmail.com>
2021-07-25 20:54:42 +0200

    add multiple changes until youtube video

--- src/app/components/map-contact/map-contact.component.ts
+++ src/app/components/map-contact/map-contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-map-contact',
+  selector: 'map-contact',
   templateUrl: './map-contact.component.html',
   styleUrls: ['./map-contact.component.scss']
 })
