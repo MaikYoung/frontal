10f664c88148171311eb9d35a014b7e512a7f063
Mike Young <maikelym1990@gmail.com>
2021-08-27 21:30:18 +0200

    add last chagnes viernes

--- src/app/pages/tecnico-superior/tecnico-superior.component.ts
+++ src/app/pages/tecnico-superior/tecnico-superior.component.ts
@@ -7,13 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoSuperiorComponent implements OnInit {
   public id:number=undefined
+  public idProgram:number=undefined
   constructor() { }
 
   ngOnInit() {
   }
 
   public openFaqs(id:number) {
-    return this.id = (this.id == undefined) ? this.id = id : this.id = undefined
+    return this.id = (this.id == undefined) ? this.id = id : this.id = id
+  }
+
+  public openProgram(id:number) {
+    return this.idProgram = (this.idProgram == undefined) ? this.idProgram = id : this.idProgram = id
   }
 
 }
